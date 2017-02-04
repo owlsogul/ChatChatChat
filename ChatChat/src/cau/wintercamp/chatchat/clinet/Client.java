00937c35ad86546e3cc701886c51fb2bf613e8f3
owlsogul <owlsogul@naver.com>
2017-02-04 23:08:30 +0900

    basic

---
+++ ChatChat/src/cau/wintercamp/chatchat/clinet/Client.java
@@ -0,0 +1,10 @@
+package cau.wintercamp.chatchat.clinet;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ChatChat/src/cau/wintercamp/chatchat/server/Server.java
@@ -0,0 +1,10 @@
+package cau.wintercamp.chatchat.server;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
