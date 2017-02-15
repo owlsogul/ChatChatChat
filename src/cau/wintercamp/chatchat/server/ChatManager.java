10aa6492d9208decabd38fa2cc8e350db758c340
simba328 <simba328@naver.com>
2017-02-15 13:27:55 +0900

    library path, ChatManager.processSelect 추가

--- src/cau/wintercamp/chatchat/server/ChatManager.java
+++ src/cau/wintercamp/chatchat/server/ChatManager.java
@@ -2,4 +2,20 @@ package cau.wintercamp.chatchat.server;
 
 public class ChatManager {
 
+	
+	String strData;
+	  //objData;
+	
+	
+	public void saveData() {
+		
+	}
+
+
+	public void whereToGo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
--- src/cau/wintercamp/chatchat/server/SocketThread.java
+++ src/cau/wintercamp/chatchat/server/SocketThread.java
@@ -4,14 +4,19 @@ package cau.wintercamp.chatchat.server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import org.json.simple.JSONObject;
 
 public class SocketThread implements Runnable{
 
-	private Socket socket;
-	BufferedReader br;
+	public Socket socket;
+	public BufferedReader br;
+	public PrintWriter pw;
+	
+	ChatManager c_manager = new ChatManager();
 	
 	public SocketThread(Socket socket) {
 		
@@ -21,26 +26,42 @@ public class SocketThread implements Runnable{
 	public void run() {
 
 		// 클라이언트로부터 데이터가 들어오면 
-		// ChatManager에 saveData() -> 데이터 저장, dataHandler() -> 데이터 처리해서 다시 어디로 보내야할 지 판별
+		// ChatManager에 saveData() -> 데이터 저장, whereToGo() -> 데이터 처리해서 다시 어디로 보내야할 지 판별
+		
+		// ChatManager로 부터 데이터 전달받으면
 		// sendData() -> 클라이언트들에게 데이터 재전송
 		
-		receiveData();
+		//receiveData();
+		
 		
 	}
 	
 	public void receiveData() {
 			
-		// 클라이언트로부터 JSON문자열 전송받음
-		br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		String data = br.readLine();
-		
-		if (data != null) {
-			ChatManager c_manager = new ChatManager(data);
+		// 클라이언트로부터 JSON문자열 전송받으면 ChatManager로 전달
+		try {
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String data = br.readLine();
+			if (data != null) {
+				
+				c_manager.saveData(data);
+				c_manager.whereToGo(data);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
-	public void sendData() {
+	public void sendData(String toClient) {
 		
+		 try {
+			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			pw.println(toClient);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void closeSocket() {
